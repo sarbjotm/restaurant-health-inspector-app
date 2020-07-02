378025e0ed3c062dba5205a18b8c29ee244bb7b4
rattlecruiser <rick_huang@sfu.ca>
2020-07-01 23:31:12 -0700

    Inspection class, InspectionManager class, TextUI
    group member: Sarbjot, Travis, Tung. made InspectionsManager class singleton.

--- app/src/test/java/Model/InspectionsManager.java
+++ app/src/test/java/Model/InspectionsManager.java
@@ -7,6 +7,18 @@ import java.util.ArrayList;
 
 public class InspectionsManager {
     private ArrayList<Inspection> inspectionList= new ArrayList<>();
+    private static InspectionsManager instance;
+
+    private InspectionsManager(){
+        
+    }
+
+    public static InspectionsManager getInstance(){
+        if (instance == null){
+            instance = new InspectionsManager();
+        }
+        return instance;
+    }
 
     public void addInspection(Inspection inspection){
         inspectionList.add(inspection);
