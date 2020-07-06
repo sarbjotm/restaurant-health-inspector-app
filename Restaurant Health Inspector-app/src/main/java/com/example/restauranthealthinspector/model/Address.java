b3e4c14f44c67dde3334dba3829aabc4b70625ef
Travis <tsc7@sfu.ca>
2020-07-06 13:34:30 -0700

    Address Class
    Create fields, getters, and constructor

---
+++ Restaurant Health Inspector-app/src/main/java/com/example/restauranthealthinspector/model/Address.java
@@ -0,0 +1,31 @@
+package com.example.restauranthealthinspector.model;
+
+public class Address {
+    private String streetAddress;
+    private String city;
+    private double latitude;
+    private double longitude;
+
+    public Address(String streetAddress, String city, double latitude, double longitude) {
+        this.streetAddress = streetAddress;
+        this.city = city;
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public String getStreetAddress() {
+        return streetAddress;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
