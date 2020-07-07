d89676bb6b8bffd466008233e385049e724ce853
Travis <tsc7@sfu.ca>
2020-07-07 16:21:11 -0700

    Remove extra input of Collection
    Added Collection too but unnecessary

--- Restaurant Health Inspector-app/src/main/java/com/example/restauranthealthinspector/model/RestaurantsManager.java
+++ Restaurant Health Inspector-app/src/main/java/com/example/restauranthealthinspector/model/RestaurantsManager.java
@@ -3,7 +3,6 @@ package com.example.restauranthealthinspector.model;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
