ce7e260637d4276807a71460b44a7e71e9c2d41f
Tung <kahmarnt@sfu.ca>
2020-07-07 11:15:07 -0700

    Branch : layout for InspectionActivity
    Both the layout for InspectionActivity and listview Violation has been done

---
+++ Restaurant Health Inspector-app/src/main/java/com/example/restauranthealthinspector/activities/InspectionActivity.java
@@ -0,0 +1,16 @@
+package com.example.restauranthealthinspector.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.restauranthealthinspector.R;
+
+public class InspectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inspection);
+    }
+}
