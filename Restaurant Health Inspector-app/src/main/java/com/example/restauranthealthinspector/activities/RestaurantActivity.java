8a8c226f77d6f9d6b10953da9105fd81291920c2
rattlecruiser <rick_huang@sfu.ca>
2020-07-07 20:10:05 -0700

    finishing 2nd activity inspections list

--- Restaurant Health Inspector-app/src/main/java/com/example/restauranthealthinspector/activities/RestaurantActivity.java
+++ Restaurant Health Inspector-app/src/main/java/com/example/restauranthealthinspector/activities/RestaurantActivity.java
@@ -5,20 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.restauranthealthinspector.R;
+import com.example.restauranthealthinspector.model.Inspection;
 import com.example.restauranthealthinspector.model.Restaurant;
 import com.example.restauranthealthinspector.model.RestaurantsManager;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
 public class RestaurantActivity extends AppCompatActivity {
     private RestaurantsManager myRestaurants;
     private Restaurant restaurant;
     private int indexRestaurant;
+    private RestaurantsManager listRestaurants;
+    ArrayList<Inspection> inspections;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,8 +40,43 @@ public class RestaurantActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        Intent intent = getIntent();
+        indexRestaurant = intent.getIntExtra("indexRestaurant", 0);
+        restaurant = myRestaurants.get(indexRestaurant);
+        inspections = restaurant.getInspectionsManager().getInspectionList();
 
         loadRestaurant();
+        populateListView();
+    }
+
+    private void populateListView(){
+        ArrayAdapter<Inspection> adapter = new MyListAdapter();
+        ListView list = (ListView) findViewById(R.id.rest_listInspections);
+        list.setAdapter(adapter);
+    }
+
+    private class MyListAdapter extends ArrayAdapter<Inspection> {
+        public MyListAdapter(){
+            super(RestaurantActivity.this, R.layout.list_inspections, inspections);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+            View itemView = convertView;
+            if (itemView == null){
+                itemView = getLayoutInflater().inflate(R.layout.list_inspections, parent, false);
+            }
+
+            Inspection currentInspection = inspections.get(position);
+            TextView numCritical = (TextView) itemView.findViewById(R.id.listI_txtCriticalNum);
+            numCritical.setText(Integer.toString(currentInspection.getNumCritical()));
+            TextView numNonCritical = (TextView) itemView.findViewById(R.id.listI_txtNonCriticalNum);
+            numNonCritical.setText(Integer.toString(currentInspection.getNumNonCritical()));
+            TextView hazardLevel = (TextView) itemView.findViewById(R.id.listI_txtHazardNum);
+            hazardLevel.setText(currentInspection.getHazardRating());
+
+            return itemView;
+        }
     }
 
     private void loadRestaurant() {
